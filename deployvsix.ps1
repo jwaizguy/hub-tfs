3ce72f69e53e584ed74199c28f440b4706ec3521
Bailey Everitt <bgeveritt@gmail.com>
2016-12-07 16:08:20 -0500

    resolved version number

--- deployvsix.ps1
+++ deployvsix.ps1
@@ -4,11 +4,16 @@ param(
 )
 
 $BaseUri = "https://updates.suite.blackducksoftware.com/repo/com/blackducksoftware/integration"
-$Project = "hub-tfs"
-$Release = "0.1.0"
-$Vsix = ("{0}-{1}.vsix" -f $Project, $Release)
-$Uri = New-Object System.Uri(("{0}/{1}/{2}/{3}" -f $BaseUri, $Project, $Release, $Vsix))
+
+# Get .vsix from current directory
+$Vsix = Get-ChildItem *.vsix
+
+$Project = ("{0}-{1}" -f $Vsix.Name.Split("-")[0], $Vsix.Name.Split("-")[1])
+$Release = $Vsix.Name.Split("-")[2].Replace(".vsix", "")
+
+$Uri = New-Object System.Uri(("{0}/{1}/{2}/{3}" -f $BaseUri, $Project, $Release, $Vsix.Name))
 $AF_Password = ConvertTo-SecureString $Password -AsPlainText -Force
 $Creds = New-Object System.Management.Automation.PSCredential ($Username, $AF_Password)
+
 # Copy to Artifactory
 Invoke-WebRequest -Uri $Uri -InFile $Vsix -Method Put -Credential $Creds
