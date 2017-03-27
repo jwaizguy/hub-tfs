554ca9338b964b49db98fd1db4d66026e19d57c7
Bailey Everitt <Bailey Everitt@DESKTOP-R23HHRL>
2017-03-27 15:34:39 -0400

    use env variable for pw

--- Tasks/tfs-scan-executor/tfs-scan-executor.ps1
+++ Tasks/tfs-scan-executor/tfs-scan-executor.ps1
@@ -237,12 +237,15 @@ Write-Host ("INFO: Project Location: {0}" -f $ScanTarget)
 Write-Host ("INFO: Project Name: {0}" -f $HubProjectName)
 Write-Host ("INFO: Project Version: {0}" -f $HubRelease)
 
+#Set BD_HUB_PASSWORD environment variable
+$env:BD_HUB_PASSWORD = $HubPassword
+
 #If a Code Location Name is specified, ensure the Hub is version 3.5.0 or later
 if ([version]$HubVersion -ge [version]"3.5.0" -and $HubCodeLocationName) {
     Write-Host ("INFO: Code Location Name: {0}" -f $HubCodeLocationName)
     Start-Process -FilePath ("{0}\bin\{1}" -f $HubScannerChildLocation, $HubScanScript) `
-	-ArgumentList ('-username {0} -password {1} -scheme {2} -host {3} -port {4} "{5}" -project "{6}" -release "{7}" -verbose -statusWriteDir "{8}" -name "{9}" -exclude /$tf/' -f `
-	$HubUsername, $HubPassword, ([System.Uri]$HubUrl).Scheme, ([System.Uri]$HubUrl).Host, ([System.Uri]$HubUrl).Port, $ScanTarget, $HubProjectName, $HubRelease, $BuildLogFolder, $HubCodeLocationName) `
+	-ArgumentList ('-username {0} -scheme {1} -host {2} -port {3} "{4}" -project "{5}" -release "{6}" -verbose -statusWriteDir "{7}" -name "{8}" -exclude /$tf/' -f `
+	$HubUsername, ([System.Uri]$HubUrl).Scheme, ([System.Uri]$HubUrl).Host, ([System.Uri]$HubUrl).Port, $ScanTarget, $HubProjectName, $HubRelease, $BuildLogFolder, $HubCodeLocationName) `
 	-NoNewWindow -Wait -RedirectStandardError (Join-Path $BuildLogFolder $LogOutput)
 }
 else {
@@ -250,8 +253,8 @@ else {
         Write-Host ("INFO: Code Location Name requires Hub 3.5.0+")
     }
     Start-Process -FilePath ("{0}\bin\{1}" -f $HubScannerChildLocation, $HubScanScript) `
-	-ArgumentList ('-username {0} -password {1} -scheme {2} -host {3} -port {4} "{5}" -project "{6}" -release "{7}" -verbose -statusWriteDir "{8}" -exclude /$tf/' -f `
-	$HubUsername, $HubPassword, ([System.Uri]$HubUrl).Scheme, ([System.Uri]$HubUrl).Host, ([System.Uri]$HubUrl).Port, $ScanTarget, $HubProjectName, $HubRelease, $BuildLogFolder) `
+	-ArgumentList ('-username {0} -scheme {1} -host {2} -port {3} "{4}" -project "{5}" -release "{6}" -verbose -statusWriteDir "{7}" -exclude /$tf/' -f `
+	$HubUsername, ([System.Uri]$HubUrl).Scheme, ([System.Uri]$HubUrl).Host, ([System.Uri]$HubUrl).Port, $ScanTarget, $HubProjectName, $HubRelease, $BuildLogFolder) `
 	-NoNewWindow -Wait -RedirectStandardError (Join-Path $BuildLogFolder $LogOutput)
 }
 
